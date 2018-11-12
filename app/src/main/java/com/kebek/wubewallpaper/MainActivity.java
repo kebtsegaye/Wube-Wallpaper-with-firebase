07abcb7b69439eef4a3b4235a8db3249482deb0d
kebFan <keburefantaye@gmail.com>
2018-11-11 19:35:31 -0500

    initial commit

---
+++ app/src/main/java/com/kebek/wubewallpaper/MainActivity.java
@@ -0,0 +1,113 @@
+package com.kebek.wubewallpaper;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.database.DatabaseReference;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    //recyclerview object
+    private RecyclerView recyclerView;
+
+    //adapter object
+    private RecyclerView.Adapter adapter;
+
+    //database reference
+    private DatabaseReference mDatabase;
+
+    //progress dialog
+    private ProgressDialog progressDialog;
+
+    //list to hold all the uploaded images and titles
+    private List<String> images;
+    private List<String> titles;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        recyclerView = findViewById(R.id.recyclerView);
+        //recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        progressDialog = new ProgressDialog(this);
+
+        images = new ArrayList<>();
+        titles = new ArrayList<>();
+
+
+        //displaying progress dialog while fetching images
+        progressDialog.setMessage("Please wait...");
+        progressDialog.show();
+
+        Toast.makeText(this, "before database reached", Toast.LENGTH_SHORT).show();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().getRoot();
+
+        // write data
+        //writeNewUser("keb", "some url");
+
+
+        //adding an event listener to fetch values
+        ValueEventListener imgAndTitleListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                //dismissing the progress dialog
+                progressDialog.dismiss();
+
+                //iterating through all the values in database
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+                    // getting the image url from firebase
+                    String url = postSnapshot.getValue(String.class);
+                    String name = postSnapshot.getKey();
+                    //uploads.add(upload);
+                    images.add(url);
+                    titles.add(name);
+                }
+
+                //creating adapter
+                adapter = new MyAdapter(getApplicationContext(), images, titles);
+
+                //adding adapter to recyclerview
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                progressDialog.dismiss();
+
+
+            }
+        };
+
+        mDatabase.addListenerForSingleValueEvent(imgAndTitleListener);
+    }
+
+    private void writeNewUser( String title, String url) {
+        Upload user = new Upload(title, url);
+        Toast.makeText(this, "user data writen", Toast.LENGTH_SHORT).show();
+        mDatabase.child("0001").child(user.getName()).setValue(url);
+
+    }
+}
---
+++ app/src/main/java/com/kebek/wubewallpaper/MyAdapter.java
@@ -0,0 +1,72 @@
+package com.kebek.wubewallpaper;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.kebek.wubewallpaper.R;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Recycler view some code shared from internate
+ */
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private Context context;
+   // private List<Upload> uploads;
+    private List<String> images;
+    private List<String> titles;
+
+
+    //public MyAdapter(Context context, List<Upload> uploads) {
+        public MyAdapter(Context context, List<String> uploads, List<String> titles) {
+
+            this.images = uploads;
+            this.titles = titles;
+            this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.layout_images, parent, false);
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        String image = images.get(position);
+        String title = titles.get(position);
+        holder.textViewName.setText(title);
+        Glide.with(context).load(image).into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView textViewName;
+        public ImageView imageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            textViewName = (TextView) itemView.findViewById(R.id.textViewName);
+            imageView = (ImageView) itemView.findViewById(R.id.imageView);
+        }
+    }
+}
